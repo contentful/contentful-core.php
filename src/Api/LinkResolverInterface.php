d595ac2990fca35b0aa61b110d7c6f7bfc888efb
Davide Borsatto <davide.borsatto@gmail.com>
2018-11-07 10:00:19 +0100

    feat(link-resolver): Add resolveLinkCollection to LinkResolverInterface

--- src/Api/LinkResolverInterface.php
+++ src/Api/LinkResolverInterface.php
@@ -22,4 +22,17 @@ interface LinkResolverInterface
      * @return ResourceInterface
      */
     public function resolveLink(Link $link, array $parameters = []): ResourceInterface;
+
+    /**
+     * Resolves an array of links.
+     * A method implementing this may apply some optimizations
+     * to reduce the amount of necessary API calls, or simply forward this
+     * to the "resolveLink" method.
+     *
+     * @param Link[]      $links
+     * @param string|null $locale
+     *
+     * @return ResourceInterface[]
+     */
+    public function resolveLinkCollection(array $links, string $locale = \null): array;
 }
