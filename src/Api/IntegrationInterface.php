59b1a854bfa86899de0aee4f9aa84b7f587e5121
Davide Borsatto <davide.borsatto@gmail.com>
2018-11-12 11:23:53 +0100

    feat(interfaces): Add ApplicationInterface and IntegrationInterface

---
+++ src/Api/IntegrationInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of the contentful/contentful-core package.
+ *
+ * @copyright 2015-2018 Contentful GmbH
+ * @license   MIT
+ */
+
+declare(strict_types=1);
+
+namespace Contentful\Core\Api;
+
+interface IntegrationInterface
+{
+    /**
+     * Returns the name of the current integration.
+     * This value must be the one that is sent as part of
+     * the "X-Contentful-User-Agent" header to the API.
+     *
+     * @return string
+     */
+    public function getIntegrationName(): string;
+
+    /**
+     * Returns the package name of the current integration.
+     * This value must be the one defined in the "composer.json" file.
+     *
+     * @return string
+     */
+    public function getIntegrationPackageName(): string;
+}
---
+++ tests/Implementation/Application.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * This file is part of the contentful/contentful-core package.
+ *
+ * @copyright 2015-2018 Contentful GmbH
+ * @license   MIT
+ */
+
+declare(strict_types=1);
+
+namespace Contentful\Tests\Core\Implementation;
+
+use Contentful\Core\Api\ApplicationInterface;
+
+class Application implements ApplicationInterface
+{
+    /**
+     * @var bool
+     */
+    private $isPackagedApplication;
+
+    public function __construct(bool $isPackagedApplication)
+    {
+        $this->isPackagedApplication = $isPackagedApplication;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getApplicationName(): string
+    {
+        return 'the-example-app';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isPackagedApplication(): bool
+    {
+        return $this->isPackagedApplication;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getApplicationPackageName(): string
+    {
+        return $this->isPackagedApplication
+            ? 'contentful/the-example-app'
+            : '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getApplicationVersion(): string
+    {
+        return $this->isPackagedApplication
+            ? ''
+            : '1.0.0';
+    }
+}
