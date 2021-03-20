e9ae4a0c10693030a1da0bb5d1311d6da16a4eef
Sebastian <sebb@sebb767.de>
2021-03-20 21:59:00 +0100

    Added copy work-arround for doc generation

--- scripts/prepare-docs.sh
+++ scripts/prepare-docs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-php scripts/sami.phar update sami.php
+cp scripts/sami.phar sami.phar
+php sami.phar update sami.php
 git checkout -qf FETCH_HEAD
 php scripts/create-redirector.php build/docs/api/index.html
 php scripts/create-redirector.php build/docs/index.html
