34d064985eab25e283853c3eb39850915a1e0cf5
Sebastian <sebb@sebb767.de>
2021-03-20 19:33:22 +0100

    Added statically built sami.phar
    This commit adds a statically built sami.phar version, which was created
    from the latest version on GitHub, since the original download server
    was shut down and therefore broke the original doc script.

--- scripts/prepare-docs.sh
+++ scripts/prepare-docs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-wget -O sami.phar http://get.sensiolabs.org/sami.phar
-php sami.phar update sami.php
+php scripts/sami.phar update sami.php
 git checkout -qf FETCH_HEAD
 php scripts/create-redirector.php build/docs/api/index.html
 php scripts/create-redirector.php build/docs/index.html
