a0147047a19841c2cbf6c0f73d29eb16790f25c1
Sebastian Kaim <sebastian.kaim@kaim-consulting.com>
2022-03-10 00:24:13 +0100

    Re-added null logger for backwards compatibility

---
+++ src/Log/NullLogger.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the contentful/contentful-core package.
+ *
+ * @copyright 2015-2022 Contentful GmbH
+ * @license   MIT
+ * @deprecated Please use the \Psr\Log\NullLogger instead.
+ */
+
+declare(strict_types=1);
+
+namespace Contentful\Core\Log;
+
+class NullLogger extends \Psr\Log\NullLogger
+{
+}
